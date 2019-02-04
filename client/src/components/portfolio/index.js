f50eb731d4498f533333fc89e4023c6874c56271
zxcv1337x <michaelhaggard94@gmail.com>
2019-02-03 21:12:18 -0600

    portfolio work started

--- client/src/components/portfolio/index.js
+++ client/src/components/portfolio/index.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
 import './portfolio.css';
-
+import { Button } from 'react-bootstrap'
+import ProjectHome from './projecthome.js'
+import projects from "./projects.json"
 
 class Portfolio extends Component {
-    render() {
-        return <div className="Portfolio">
-            TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST
-        </div>
+    constructor() {
+        super() 
+        this.state = {
+            projectName: "",
+            projectDesc: "",
+            projectPicture: "",
+            projectDemo: "",
+            projectRepo: "",
+            isHome: true
+        }
     }
-}
 
-export default Portfolio;
+
+    render() {
+        return( <div className="Portfolio">
+        <div className="Project">
+        {this.state.isHome ? <ProjectHome />
+        : ""}
+        </div>
+        <div className="Sidebar">
+        <Button>Portfolio Home</Button>
+        <Button>Aestheticize</Button>
+        <Button>CatchAll</Button>
+        <Button>Test Button</Button>
+        </div>
+        
+        </div>
+        
+        
+            )}
+    }
+  
+
+  export default Portfolio;
---
+++ client/src/components/portfolio/projecthome.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+
+
+class ProjectHome extends Component {
+    render() {
+        return <div>
+
+            <h1>Education</h1>
+            <ul>
+                <li>University of Kansas (2018-2019)</li>
+                    <ul>
+                        <li>Full Stack Web Development Bootcamp</li>
+                    </ul>
+                <li>Pioneer Technology Center (2017-2018)</li> 
+                    <ul>
+                        <li>CCNA Training</li>
+                    </ul>
+                <li>Northern Oklahoma College (2013-2015)</li>
+                    <ul>
+                        <li>Associate's in Computer Science</li>
+                    </ul>
+            </ul>
+
+
+            <h1>Skills - Technologies</h1>
+            <h5>I specialize in the MERN stack and general JavaScript work.</h5>
+            <h5>Here are a list of relevant programming technologies I consider myself skilled in:</h5>
+            <ul>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>SQL</li>
+                <li>MongoDB</li>
+                <li>HTML/CSS (Bootstrap)</li>
+                <li>Handlebars</li>
+                <li>jQuery</li>
+                <li>Git</li>
+            </ul>
+            <h5>Other programming technologies I have experience with to some degree:</h5>
+            <ul>
+                <li>Ruby on Rails</li>
+                <li>Java</li>
+                <li>C</li>
+                <li>Visual Basic</li>
+                <li>UNIX</li>
+            </ul>
+            <h5>Computer related technologies that aren't necessarily programming related:</h5>
+            <ul>
+                <li>Microsoft Office (Word, Excel, Powerpoint, Access, Outlook)</li>
+                <li>Cisco Routing & Networking</li>
+                <li>Cellular Networks</li>
+                <li>Wordpress</li>
+                <li>Social media</li>
+            </ul>
+            <h1>Skills - Other</h1>
+            <h5>Skills, subjects and concepts that aren't particularly relevant to my career:</h5>
+            <ul>
+                <li>Creative writing</li>
+                <li>Basic accounting</li>
+                <li>Macroeconomics</li>
+                <li>Microeconomics</li>
+                <li>Business applications</li>
+                <li>Marketing</li>
+            </ul>
+        </div>
+    }
+}
+
+export default ProjectHome;
