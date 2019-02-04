09bdc25595912f6b9a5e45bd47bb3b7daf281ed9
zxcv1337x <michaelhaggard94@gmail.com>
2019-02-03 18:48:51 -0600

    blog link

--- client/src/components/blog/index.js
+++ client/src/components/blog/index.js
@@ -5,7 +5,9 @@ import './blog.css';
 class Blog extends Component {
     render() {
         return <div className="Blog">
-  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST  TEST
+        <p>I like music a lot, and not just listening to it. I've got a music blog (that also hosts my writings) where I review and share music.
+        For now, my blog is hosted on Wordpress. I'll be migrating it to my own site sometime in the future, maybe. Check it out <a href="http://teamzxcv.com">here.</a></p>
+    
         </div>
     }
 }
