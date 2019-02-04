4b3a49127962642185af43a9c5e1b9a23a206d46
zxcv1337x <michaelhaggard94@gmail.com>
2019-02-04 12:44:21 -0600

    portfolio page done

--- client/src/components/portfolio/index.js
+++ client/src/components/portfolio/index.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import './portfolio.css';
 import { Button } from 'react-bootstrap'
 import ProjectHome from './projecthome.js'
+import ProjectPage from './projectpage.js'
 import projects from "./projects.json"
+
 
 class Portfolio extends Component {
     constructor() {
@@ -15,20 +17,44 @@ class Portfolio extends Component {
             projectRepo: "",
             isHome: true
         }
+        this.portfolioChange = this.portfolioChange.bind(this)
     }
 
+    portfolioChange(x) {
+        if(x === "home") {
+            this.setState({
+                isHome: true
+            })
+        } else {
+            let data = projects[x]
+            this.setState({
+                isHome: false,
+                projectName: data.name,
+                projectDesc: data.description,
+                projectPicture: data.picture,
+                projectDemo: data.demo,
+                projectRepo: data.repo
+            })
+        }
+    }
 
     render() {
         return( <div className="Portfolio">
         <div className="Project">
         {this.state.isHome ? <ProjectHome />
-        : ""}
+        : <ProjectPage name={this.state.projectName} description={this.state.projectDesc}
+            picture={this.state.projectPicture} demo={this.state.projectDemo} repo={this.state.projectRepo} /> }
         </div>
         <div className="Sidebar">
-        <Button>Portfolio Home</Button>
-        <Button>Aestheticize</Button>
-        <Button>CatchAll</Button>
-        <Button>Test Button</Button>
+        <Button onClick={() => this.portfolioChange("home")}>Portfolio Home</Button>
+        <Button onClick={() => this.portfolioChange(0)}>Aestheticize</Button>
+        <Button onClick={() => this.portfolioChange(1)}>CatchAll</Button>
+        <Button onClick={() => this.portfolioChange(2)}>WiredIn</Button>
+        <Button onClick={() => this.portfolioChange(3)}>NYT Scraper</Button>
+        <Button onClick={() => this.portfolioChange(4)}>Eat-Da-Burger!</Button>
+        <Button onClick={() => this.portfolioChange(5)}>Friend Finder</Button>
+        <Button onClick={() => this.portfolioChange(6)}>Bamazon</Button>
+        <Button onClick={() => this.portfolioChange(7)}>LIRI</Button>
         </div>
         
         </div>
---
+++ client/src/components/portfolio/projectpage.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import './projectpage.css';
+import github from '../footer/github.png'
+
+class ProjectPage extends Component {
+    constructor(props) {
+        super(props)
+    }
+  
+    render() {
+        let name = this.props.name;
+        let desc = this.props.description;
+        let pic = this.props.picture;
+        let demo = this.props.demo;
+        let repo = this.props.repo;
+
+        return ( 
+            <div className="Project">
+            <div className="Project-img">
+            <img src={pic} alt="Project Picture" />
+            </div>
+            <div className="Project-top">
+                <div className="Name">{name} <a href={repo} className="ghicon"><img src={github} alt="Github"/></a></div>
+                <div className="Project-subheaders"> 
+                {demo ? <a href={demo}>Demo</a> : "Demo unavailable"} 
+                <a href={repo} className="ghlink">Github</a></div>
+            </div>
+            <div className="Project-words">
+            <p>
+            {desc}
+            </p>
+            </div>
+
+        </div>
+        )}
+}
+
+export default ProjectPage;
