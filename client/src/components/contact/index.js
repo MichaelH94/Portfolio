759eaff21c379d954bed55ac314111da1614c43a
zxcv1337x <michaelhaggard94@gmail.com>
2019-02-15 12:59:29 -0600

    fix

--- client/src/components/contact/index.js
+++ client/src/components/contact/index.js
@@ -11,7 +11,7 @@ class Contact extends Component {
         const message = document.getElementById('message').value;
         axios({
             method: "POST", 
-            url:"http://localhost:3000/send", 
+            url:"http://mjh-portfolio.herokuapp.com/send", 
             data: {
                 name: name,   
                 email: email,  
