a29b542317eaf558c119671d8f69c00742f97f56
zxcv1337x <michaelhaggard94@gmail.com>
2019-06-15 15:38:12 -0500

    fixing some things

--- client/src/components/blog/index.js
+++ client/src/components/blog/index.js
@@ -6,9 +6,10 @@ class Blog extends Component {
     render() {
         return <div className="Blog">
         <p>I like music a lot, and not just listening to it. I've got a blog where I primarily review and share music. In addition to music, this site also hosts my creative writing, as well as
-        various culture and tech related opinion pieces. For now, my blog is hosted on Wordpress. I'll be migrating it to my own site sometime in the future, maybe. Check it out <a href="http://teamzxcv.com">here.</a></p>
-        
-        I also have a RateYourMusic page <a href="https://rateyourmusic.com/~zxcv1337x">here</a>, if you'd like to read my thoughts on music.
+        various culture and tech related opinion pieces. For now, my blog is hosted on Wordpress. I'll be migrating it to my own site sometime in the future, maybe. Check it out <a href="http://teamzxcv.com">here.</a>
+        <br/>
+        <br/>
+        I also have a RateYourMusic page <a href="https://rateyourmusic.com/~zxcv1337x">here</a>, if you'd like to read my thoughts on music.</p>
         </div>
     }
 }
