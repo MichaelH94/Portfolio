a7da5f35f3201e866984d7533d2278a52b27769b
zxcv1337x <michaelhaggard94@gmail.com>
2020-09-08 23:29:19 -0500

    slight changes

--- client/src/components/portfolio/index.js
+++ client/src/components/portfolio/index.js
@@ -49,7 +49,7 @@ class Portfolio extends Component {
         </div>
         <div className="Sidebar">
         <Button type="home" onClick={() => this.portfolioChange("home")}>Portfolio Home</Button>
-        <Button type="j" onClick={() => this.portfolioChange(12)}>MikeHub</Button>
+        <Button type="js" onClick={() => this.portfolioChange(12)}>MikeHub</Button>
         <Button type="js" onClick={() => this.portfolioChange(0)}>Aestheticize</Button>
         <Button type="js" onClick={() => this.portfolioChange(11)}>zxcvBot</Button> 
         <Button type="js" onClick={() => this.portfolioChange(1)}>Catch-All</Button>
